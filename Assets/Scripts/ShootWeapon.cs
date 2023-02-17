7f7055e1cd49d3c11f87986c819940580ca91c9c
Cleo Trumbull <79284274+cleo-t@users.noreply.github.com>
2023-02-17 15:48:44 -0500

    shooting script

---
+++ Assets/Scripts/ShootWeapon.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootWeapon : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 100;
+    public AudioClip shootSFX;
+
+    public Image CrosshairImage;
+
+    Color originalReticleColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectile =
+                Instantiate(
+                    projectilePrefab,
+                    transform.position + transform.forward,
+                    transform.rotation
+                ) as GameObject;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * projectileSpeed, ForceMode.VelocityChange);
+
+            projectile.transform.SetParent(
+                GameObject.FindGameObjectWithTag("ProjectileParent").transform
+            );
+
+            AudioSource.PlayClipAtPoint(shootSFX, transform.position);
+        }
+    }
+}
