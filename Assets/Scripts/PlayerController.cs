ee900eff522bda13d0357489b5fad241b5665665
Kevin Liu <kliu0932@gmail.com>
2023-02-20 16:10:43 -0500

    fixed shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 2f;
-
+    public float gravity = 9.81f;
+    public float airControl = 10f;
+    Vector3 moveDirection;
     CharacterController controller;
 
     // gets the character controller
@@ -19,11 +21,22 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        
+
         Vector3 controlInput = playerSpeed *
             (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
 
-        controller.Move(controlInput * Time.deltaTime);
-        
+        if (controller.isGrounded)
+        {
+            moveDirection = controlInput;
+        }
+        else
+        {
+            controlInput.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, controlInput, airControl * Time.deltaTime);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
     }
 }
