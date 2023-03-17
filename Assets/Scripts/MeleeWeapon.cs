c750c94accd5cb552d9919e57227371697170b8b
KobeZ123 <kobezhe@gmail.com>
2023-03-17 14:00:48 -0400

    big stick swinging and melee

--- Assets/Scripts/MeleeWeapon.cs
+++ Assets/Scripts/MeleeWeapon.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class MeleeWeapon : MonoBehaviour
 {
     public int damageAmount = 10;
+    public AudioClip swingSFX;
+
     Animator anim;
     float elapsedTime = 0f;
+    
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1") && InventoryManager.currentWeaponPrefab.CompareTag("Melee"))
         {
+
             anim = InventoryManager.currentWeaponPrefab.GetComponent<Animator>();
             anim.SetBool("isHitting", true);
             elapsedTime += Time.deltaTime;
 
+            // TODO: find location to put this with cooldown 
+            AudioSource.PlayClipAtPoint(swingSFX, Camera.main.transform.position);
 
             if (anim.GetBool("isHitting"))
             {
                 if (elapsedTime < anim.GetCurrentAnimatorStateInfo(0).length)
                 {
+                    
                     elapsedTime += Time.deltaTime;
                 }
                 else
                 {
+                    
                     elapsedTime = 0.0f;
                     anim.SetBool("isHitting", false);
                 }
