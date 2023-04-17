7d3bf8921f2f0c27b12da52ea65e81f1ecb8f39e
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-04-17 14:58:41 -0400

    merge fixed

--- Assets/IntroSceneLevelNext.cs
+++ Assets/IntroSceneLevelNext.cs
@@ -10,11 +10,7 @@ public class IntroSceneLevelNext : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.E)) {
             Debug.Log("going to " + LevelScriptableObject.currentScene);
-<<<<<<< Updated upstream
-            SceneManager.LoadScene(LevelScriptableObject.currentScene);//LevelScriptableObject.currentScene);
-=======
-            SceneManager.LoadScene(0);//LevelScriptableObject.currentScene);
->>>>>>> Stashed changes
+            SceneManager.LoadScene(LevelScriptableObject.currentScene);
         }
     }
 }
